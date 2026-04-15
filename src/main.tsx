ef7d5d960e3977cd3a9da0a42b67c388011993bc
Constant <constantine.ru@gmail.com>
2026-04-15 23:57:51 +0500

    Preloader

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,18 @@ import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.tsx';
 
+// Declare global window type for hidePreloader
+declare global {
+  interface Window {
+    hidePreloader?: () => void;
+  }
+}
+
+// Hide preloader after React starts rendering
+if (typeof window.hidePreloader === 'function') {
+  window.hidePreloader();
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
